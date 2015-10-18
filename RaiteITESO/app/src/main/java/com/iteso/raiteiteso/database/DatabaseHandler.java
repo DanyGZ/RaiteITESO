2c42568c2cfacb0b18e7b9bf26333733348fc0c2
Daniel Ignacio García Zúñiga <dani_nacho.95@hotmail.com>
2015-10-18 13:17:08 -0500

    Test

--- RaiteITESO/app/src/main/java/com/iteso/raiteiteso/database/DatabaseHandler.java
+++ RaiteITESO/app/src/main/java/com/iteso/raiteiteso/database/DatabaseHandler.java
@@ -1,7 +1,28 @@
 package com.iteso.raiteiteso.database;
 
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
 /**
  * Created by Daniel on 18/10/2015.
  */
-public class DatabaseHandler {
+public class DatabaseHandler extends SQLiteOpenHelper{
+
+    private static final String DATABASE_NAME = "RaiteITESO.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHandler(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
 }
