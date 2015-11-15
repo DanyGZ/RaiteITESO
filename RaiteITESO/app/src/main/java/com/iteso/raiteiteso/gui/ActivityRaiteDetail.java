91d59ef2bd89816f0348856d272a99e365f361f0
houstonsalgado <houstonsistemas@gmail.com>
2015-11-15 14:19:47 -0700

    Si truena algo me dices

--- RaiteITESO/app/src/main/java/com/iteso/raiteiteso/gui/ActivityRaiteDetail.java
+++ RaiteITESO/app/src/main/java/com/iteso/raiteiteso/gui/ActivityRaiteDetail.java
@@ -1,7 +1,27 @@
 package com.iteso.raiteiteso.gui;
 
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.ListView;
+
+import java.util.ArrayList;
+
 /**
  * Created by Daniel on 29/10/2015.
  */
-public class ActivityRaiteDetail {
+public class ActivityRaiteDetail extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_raite_list);
+
+        ArrayList<String> detail = new ArrayList<>();
+
+        AdapterListRaiteDetail raiteDetail = new AdapterListRaiteDetail(detail, this);
+        ListView listDetail = (ListView)findViewById(R.id.activity_raite_detail_list);
+        listDetail.setAdapter(raiteDetail);
+
+    }
+
 }
