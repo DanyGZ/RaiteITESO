a23e7eb97240b905c9f91b78b486cee823f662c0
Daniel Ignacio García Zúñiga <dani_nacho.95@hotmail.com>
2015-11-19 07:30:42 -0600

    Create account

--- RaiteITESO/app/src/main/java/com/iteso/raiteiteso/gui/ActivityCreateAccount.java
+++ RaiteITESO/app/src/main/java/com/iteso/raiteiteso/gui/ActivityCreateAccount.java
@@ -1,6 +1,7 @@
 package com.iteso.raiteiteso.gui;
 
 import android.app.Activity;
+import android.app.TimePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +9,7 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TimePicker;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -16,12 +18,15 @@ import com.iteso.raiteiteso.beans.UserWOCar;
 import com.iteso.raiteiteso.database.DatabaseHandler;
 import com.iteso.raiteiteso.database.UserControl;
 
+import java.util.Calendar;
+
 /**
  * Created by Daniel on 25/10/2015.
  */
 public class ActivityCreateAccount extends Activity {
     private EditText userNameEditText;
     private EditText nameEditText;
+    private EditText lastNameeditText;
     private EditText passwordEditText;
     private EditText repeatEditText;
     private EditText mondayEditText;
@@ -38,6 +43,23 @@ public class ActivityCreateAccount extends Activity {
     private LinearLayout carLayout;
     private DatabaseHandler dh;
     private UserControl userControl;
+    private Button firstContainerNext;
+    private String userName;
+    private String lastName;
+    private String name;
+    private String password;
+    private String repeatedPassword;
+    private String monday;
+    private String tuesday;
+    private String wednesday;
+    private String thursday;
+    private String friday;
+    private String car;
+    private String carColor;
+    private String carCapacity;
+    private LinearLayout firstContainer;
+    private LinearLayout secondContainer;
+    private Button back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -63,6 +85,19 @@ public class ActivityCreateAccount extends Activity {
         availableToggle = (ToggleButton) findViewById(R.id.activity_create_account_toggle_available);
         createActount = (Button) findViewById(R.id.activity_create_account_create);
         carLayout = (LinearLayout) findViewById(R.id.activity_create_account_layout_car);
+        firstContainerNext = (Button) findViewById(R.id.activity_create_account_next);
+        lastNameeditText = (EditText) findViewById(R.id.activity_create_account_last_name);
+        firstContainer = (LinearLayout) findViewById(R.id.activity_create_account_first_data_container);
+        secondContainer = (LinearLayout) findViewById(R.id.activity_create_account_second_data_container);
+        back = (Button) findViewById(R.id.activity_create_account_back);
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                firstContainer.setVisibility(View.VISIBLE);
+                secondContainer.setVisibility(View.INVISIBLE);
+            }
+        });
 
         carToggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
@@ -75,32 +110,149 @@ public class ActivityCreateAccount extends Activity {
             }
         });
 
+        mondayEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                final int minute = mcurrentTime.get(Calendar.MINUTE);
+                final TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(ActivityCreateAccount.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        mondayEditText.setText(selectedHour + " : " + selectedMinute);
+                    }
+                }, hour, minute, false);
+                mTimePicker.setTitle("Seleccionar hora");
+                mTimePicker.show();
+
+            }
+        });
+
+        tuesdayEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                final int minute = mcurrentTime.get(Calendar.MINUTE);
+                final TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(ActivityCreateAccount.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        tuesdayEditText.setText(selectedHour + " : " + selectedMinute);
+                    }
+                }, hour, minute, false);
+                mTimePicker.setTitle("Seleccionar hora");
+                mTimePicker.show();
+
+            }
+        });
+
+        wednesdayEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                final int minute = mcurrentTime.get(Calendar.MINUTE);
+                final TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(ActivityCreateAccount.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        wednesdayEditText.setText(selectedHour + " : " + selectedMinute);
+                    }
+                }, hour, minute, false);
+                mTimePicker.setTitle("Seleccionar hora");
+                mTimePicker.show();
+
+            }
+        });
+
+        thursdayEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                final int minute = mcurrentTime.get(Calendar.MINUTE);
+                final TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(ActivityCreateAccount.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        thursdayEditText.setText(selectedHour + " : " + selectedMinute);
+                    }
+                }, hour, minute, false);
+                mTimePicker.setTitle("Seleccionar hora");
+                mTimePicker.show();
+
+            }
+        });
+
+        fridayEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Calendar mcurrentTime = Calendar.getInstance();
+                int hour = mcurrentTime.get(Calendar.HOUR_OF_DAY);
+                final int minute = mcurrentTime.get(Calendar.MINUTE);
+                final TimePickerDialog mTimePicker;
+                mTimePicker = new TimePickerDialog(ActivityCreateAccount.this, new TimePickerDialog.OnTimeSetListener() {
+                    @Override
+                    public void onTimeSet(TimePicker timePicker, int selectedHour, int selectedMinute) {
+                        fridayEditText.setText(selectedHour + " : " + selectedMinute);
+                    }
+                }, hour, minute, false);
+                mTimePicker.setTitle("Seleccionar hora");
+                mTimePicker.show();
+
+            }
+        });
+
+        firstContainerNext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                userName = userNameEditText.getText().toString();
+                name = nameEditText.getText().toString();
+                lastName = lastNameeditText.getText().toString();
+                password = passwordEditText.getText().toString();
+                repeatedPassword = repeatEditText.getText().toString();
+
+                if (userName.equals("") || name.equals("") || password.equals("") || repeatedPassword.equals("") ||
+                        lastName.equals("")) {
+                    Toast.makeText(ActivityCreateAccount.this, "Debe llenar todos los campos", Toast.LENGTH_LONG).show();
+                } else {
+                    if (password.equals(repeatedPassword)) {
+                        if (userControl.getUserWithCarByUserName(userName, dh) == null &&
+                                userControl.getUserWithOuthCarByUserName(userName, dh) == null) {
+                            firstContainer.setVisibility(View.GONE);
+                            secondContainer.setVisibility(View.VISIBLE);
+                        } else {
+                            Toast.makeText(ActivityCreateAccount.this, "Nombre de usuario ya existente", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(ActivityCreateAccount.this, "Las contraseñas no coinciden", Toast.LENGTH_LONG).show();
+                    }
+                }
+            }
+        });
+
         createActount.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 UserWCar user;
 
-                String userName = userNameEditText.getText().toString();
-                String name = nameEditText.getText().toString();
-                String password = passwordEditText.getText().toString();
-                String repeatedPassword = repeatEditText.getText().toString();
-                String monday = mondayEditText.getText().toString();
-                String tuesday = tuesdayEditText.getText().toString();
-                String wednesday = wednesdayEditText.getText().toString();
-                String thursday = thursdayEditText.getText().toString();
-                String friday = fridayEditText.getText().toString();
-                String car = carEditText.getText().toString();
-                String carColor = carColorEditText.getText().toString();
-                String carCapacity = carCapacityEditText.getText().toString();
+                monday = mondayEditText.getText().toString();
+                tuesday = tuesdayEditText.getText().toString();
+                wednesday = wednesdayEditText.getText().toString();
+                thursday = thursdayEditText.getText().toString();
+                friday = fridayEditText.getText().toString();
+                car = carEditText.getText().toString();
+                carColor = carColorEditText.getText().toString();
+                carCapacity = carCapacityEditText.getText().toString();
 
 
-                if(userName.equals("") || name.equals("") || password.equals("") || repeatedPassword.equals("") ||
-                        monday.equals("") || tuesday.equals("") || wednesday.equals("") || thursday.equals("") ||
+                if(monday.equals("") || tuesday.equals("") || wednesday.equals("") || thursday.equals("") ||
                         friday.equals("") || (car.equals("") && carToggle.isChecked()) ||
                         (carColor.equals("") && carToggle.isChecked()) || (carCapacity.equals("") && carToggle.isChecked())){
                     Toast.makeText(ActivityCreateAccount.this, "Debe llenar todos los campos", Toast.LENGTH_LONG).show();
                 }else{
-                    if(password.equals(repeatedPassword)){
                         user = new UserWCar();
                         user.setUserName(userName);
                         user.setName(name);
@@ -132,9 +284,6 @@ public class ActivityCreateAccount extends Activity {
                             }
                         }
 
-                    }else{
-                        Toast.makeText(ActivityCreateAccount.this, "Las contraseñas no coinciden", Toast.LENGTH_LONG).show();
-                    }
                 }
             }
         });
