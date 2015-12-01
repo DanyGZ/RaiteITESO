7657de8a37efad76c636c5a6f14cb405d4346162
GIOVANNI <giovannizunoalcazar@gmail.com>
2015-12-01 15:08:29 -0600

    Avance con Tests de text

---
+++ RaiteITESO/app/src/androidTest/java/com/iteso/raiteiteso/ActMainWithCar.java
@@ -0,0 +1,79 @@
+package com.iteso.raiteiteso;
+
+import android.test.ActivityInstrumentationTestCase2;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.iteso.raiteiteso.gui.ActivityLogin;
+import com.iteso.raiteiteso.gui.R;
+
+/**
+ * Created by giovanni on 27/11/2015.
+ */
+public class ActMainWithCar extends ActivityInstrumentationTestCase2<ActivityLogin>{
+    private ActivityLogin activityLogin;
+    private TextView titleTextView;
+    private Button logInButton;
+    private Button signUpButton;
+    private EditText userEditText;
+    private EditText passwordEditText;
+    private ImageView logoImageView;
+
+    public ActMainWithCar() {
+        super(ActivityLogin.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception{
+        super.setUp();
+        setActivityInitialTouchMode(true);
+
+        activityLogin = getActivity();
+        titleTextView = (TextView) activityLogin.findViewById(R.id.activity_login_title);
+        logInButton = (Button) activityLogin.findViewById(R.id.activity_login_login);
+        signUpButton = (Button) activityLogin.findViewById(R.id.activity_login_create);
+        userEditText = (EditText) activityLogin.findViewById(R.id.activity_login_user);
+        passwordEditText = (EditText) activityLogin.findViewById(R.id.activity_login_password);
+        logoImageView = (ImageView) activityLogin.findViewById(R.id.activity_login_logo);
+    }
+
+    public void testPreconditions() {
+        assertNotNull("activityLogin is null", activityLogin);
+        assertNotNull("titleTextView is null", titleTextView);
+        assertNotNull("logInButton is null", logInButton);
+        assertNotNull("signUpButton is null", signUpButton);
+        assertNotNull("userEditText is null", userEditText);
+        assertNotNull("passwordEditText is null", passwordEditText);
+        assertNotNull("logoImageView is null", logoImageView);
+    }
+
+    public void testTitleTextView_text() {
+        final String expected = "Ride ITESO";
+        final String actual = titleTextView.getText().toString();
+        assertEquals("titleTextView contains wrong text", expected, actual);
+    }
+
+    public void testLogInButton_labelText() {
+        final String expectedLoginButtonText = "Log In";
+        final String actualLoginButtonText = logInButton.getText().toString();
+        assertEquals("logInButton contains wrong text", expectedLoginButtonText, actualLoginButtonText);
+    }
+
+    public void testSignUpButton_labelText() {
+        final String expectedSignUpButtonText = "Sign Up";
+        final String actualSignUpButtonText = signUpButton.getText().toString();
+        assertEquals("signUpButton contains wrong text", expectedSignUpButtonText, actualSignUpButtonText);
+    }
+
+    public void testUserEditText_empty() {
+        final String actualUserEditTextText = userEditText.getText().toString();
+        assertEquals("userEditText isn't empty", "", actualUserEditTextText);
+    }
+
+    public void testPasswordEditText_empty() {
+        final String actualPasswordEditTextText = passwordEditText.getText().toString();
+        assertEquals("userEditText isn't empty", "", actualPasswordEditTextText);
+    }
+}
